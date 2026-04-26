999188a3dba4a864eab89b13f1c4f31119c7f8dc
brunnacleite-afk <brunnacleite@gmail.com>
2026-04-25 21:31:29 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -17,3 +17,9 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+numero = int(input("Digite um número:"))
+if numero % 2 == 0:
+    print("Seu número é par")
+else:
+    print("Seu número é ímpar")
